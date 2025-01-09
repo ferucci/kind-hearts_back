0553954f01fff17464d92cb54316e1158d722755
Ferucci <development.first1@gmail.com>
2025-01-09 15:19:29 +0300

    war with cors 2

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -4,29 +4,8 @@ export default [
   {
     name: 'strapi::security',
     config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:', 'http:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'https:', 'http:'],
-          'media-src': ["'self'", 'data:', 'blob:', 'https:', 'http:'],
-          upgradeInsecureRequests: null,
-        },
-      },
-      cors: {
-        enabled: true,
-        origin: ['https://www.khcharity.com', 'https://khcharity.com', 'http://localhost:5173'],
-        headers: [
-          'Content-Type',
-          'Authorization',
-          'X-Frame-Options',
-          'Origin',
-          'Accept',
-        ],
-        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-        credentials: true,
-        maxAge: 3600
-      },
+      contentSecurityPolicy: false,
+      cors: false
     },
   },
   {
