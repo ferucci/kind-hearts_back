55dab7ed562aec852ade4cfe15bf9d53143c45b3
Ferucci <development.first1@gmail.com>
2025-01-09 12:11:53 +0300

    changed the logic in middleware (cors)

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,14 +2,19 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['www.khcharity.com', 'khcharity.com'],
+      methods: ['GET', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-  {
-    name: "global::custom-cors",
-  },
 ];
