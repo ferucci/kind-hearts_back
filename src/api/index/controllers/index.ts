d4b96cda905860311637e0b927c30fbb17147e45
Ferucci <development.first1@gmail.com>
2025-01-10 15:40:34 +0300

    test header from strapi

--- src/api/index/controllers/index.ts
+++ src/api/index/controllers/index.ts
@@ -5,7 +5,7 @@
 import { factories } from '@strapi/strapi'
 
 const allowedOrigins = [
-  'http://0.0.0.0:1337',
+  'http://http://localhost:5173/',
   'http://api.khcharity.com',
   'http://khcharity.com',
   'https://api.khcharity.com',
@@ -14,8 +14,18 @@ const allowedOrigins = [
 
 export default factories.createCoreController('api::index.index', ({ strapi }) => ({
   async find(ctx) {
-    ctx.set('Access-Control-Allow-Origin', '*');
+    const origin = ctx.request.header.origin;
+    if (allowedOrigins.includes(origin)) {
+      ctx.set('Access-Control-Allow-Origin', origin);
+    }
     const response = await super.find(ctx);
+
+    ctx.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    ctx.set('Access-Control-Allow-Headers', 'DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range,Authorization');
+    ctx.set('Access-Control-Allow-Credentials', 'true');
+    ctx.set('Access-Control-Expose-Headers', 'Content-Length,Content-Range');
+    ctx.set('Access-Control-Max-Age', '86400');
+
     return response;
   }
 }));
